9fb3cadbac239d9818d560e7cb667298a265a03b
Manu S Rao <67941652+MANUSRAO@users.noreply.github.com>
2023-03-08 13:29:41 +0530

    Update settings.py

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['172.17.10.180','localhost','172.17.8.228','.vercel.app','.com'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'manusrao.is20@rvce.edu.in'
-EMAIL_HOST_PASSWORD = 'uzu1@buntu'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
